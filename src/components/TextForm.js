bc0dfd7dc2a5bffb8e309c0f7c2c09275936c7a1
vikas jadaun <deadpoolji125@gmail.com>
2023-07-14 13:15:11 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function TextForm() {
   return (
-   <>
-    <div className="mb-3">
-  <label for="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-</div>
-<div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-   </>
-  )
+    <>
+      <div className="mb-3">
+        <label for="exampleFormControlTextarea1" className="form-label">
+          Example textarea
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+    </>
+  );
 }

ebb746f2fa80640159cef285225ae0e7b4c27567
vikas jadaun <deadpoolji125@gmail.com>
2023-07-14 13:21:44 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,12 @@
 import React from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
   return (
     <>
+    <h1>{props.heading}</h1>
       <div className="mb-3">
-        <label for="my-box" className="form-label">
-          Example textarea
-        </label>
-        <textarea
-          className="form-control"
-          id="mybox"
-          rows="3"
-        ></textarea>
+        <label for="my-box" className="form-label">Example textarea</label>
+        <textarea className="form-control" id="mybox" rows="3"></textarea>
       </div>
     </>
   );

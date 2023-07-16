c993486bd5318c484ee1faf5efc2b41d42341cef
vikas jadaun <deadpoolji125@gmail.com>
2023-07-16 18:42:02 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,12 +2,11 @@ import React from "react";
 
 export default function TextForm(props) {
   return (
-    <>
-    <h1>{props.heading}</h1>
+    <div>
+      <h1>{props.heading}</h1>
       <div className="mb-3">
-       
         <textarea className="form-control" id="mybox" rows="8"></textarea>
       </div>
-    </>
+    </div>
   );
 }

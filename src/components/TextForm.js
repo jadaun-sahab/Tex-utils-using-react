2653f5a730cbf1231817f3a53e2c3bdae95542b6
vikas jadaun <deadpoolji125@gmail.com>
2023-07-17 10:23:10 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,9 +4,9 @@ export default function TextForm(props) {
   const [text, setText] = useState('Enter text here');
   return (
     <div>
-      <h1>{props.heading} - {text}</h1>
+      <h1>{props.heading} </h1>
       <div className="mb-3">
-        <textarea className="form-control" id="mybox" rows="8"></textarea>
+        <textarea className="form-control" value={text} id="mybox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary">convert to uppercase</button>
     </div>

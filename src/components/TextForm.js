fcfb4048bc5f8c4c90af22aa4a82338276cd9659
vikas jadaun <deadpoolji125@gmail.com>
2023-07-19 19:29:09 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,7 +8,7 @@ export default function TextForm(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text} id="mybox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary">Convert to uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
     </div>
   );
 }

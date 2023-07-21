9e1a777ce5d3fff02802ebaf29116fc3bf6b7794
vikas jadaun <deadpoolji125@gmail.com>
2023-07-22 00:27:58 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,20 +11,16 @@ export default function TextForm(props) {
   };
   const [text, setText] = useState("Enter Text Here");
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading} </h1>
       <div className="mb-3">
         <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="mybox"
-          rows="8"
-        ></textarea>
+          className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to uppercase
-      </button>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
     </div>
+    </>
+    
   );
 }

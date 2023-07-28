84718c995252a6568b001705d8a263cebe1af930
vikas jadaun <deadpoolji125@gmail.com>
2023-07-28 12:15:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode , setMode] = useState('light');
   return (
     <>
       
-<Navbar title="Text-Utils" abouttext="About us"/> 
+<Navbar title="Text-Utils" abouttext="About us" mode={mode}/> 
 <div className="container my-3">
 
 <TextForm heading="Enter The Text Here To Analyze"/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-dark`}>
      <div className="container-fluid">
        <a className="navbar-brand" href="/">{props.title}</a>
        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

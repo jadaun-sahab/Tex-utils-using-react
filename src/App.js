1074b739727d09378fa2bdec8e3a635bb1ecd948
vikas jadaun <deadpoolji125@gmail.com>
2023-07-28 20:44:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
-  const [mode , setMode] = useState('light');
+  const [mode , setMode] = useState('dark') ;
+  const toggleMode = () =>{
+    if(mode ==='light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
   return (
     <>
       
-<Navbar title="Text-Utils" abouttext="About us" mode={mode}/> 
+<Navbar title="Text-Utils" abouttext="About us" mode={mode} toggleMode={toggleMode} /> 
 <div className="container my-3">
 
 <TextForm heading="Enter The Text Here To Analyze"/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar(props) {
              <a className="nav-link" href="/">{props.abouttext}</a>
            </li>
          </ul>
-         <div className="form-check form-switch">
+         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
           <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
           <label className="form-check-label" htmlForfor="flexSwitchCheckDefault">Enable Dark Mode</label>
 </div>

56bd7b71bd993eef87de9c619677289308ca5b73
vikas jadaun <deadpoolji125@gmail.com>
2023-07-30 11:46:17 +0530

    UPDATE

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export default function Alert(props) {
   return (
     <div>
      <div className="alert alert-warning alert-dismissible fade show" role="alert">
-          <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+          <strong>Welcome To My website</strong>
           <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
      </div>
     </div>

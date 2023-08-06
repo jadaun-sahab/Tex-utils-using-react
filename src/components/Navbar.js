8d3d3713ab992cdecd556a20b64c459d292fb7cf
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-06 15:59:55 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,9 +15,9 @@ export default function Navbar(props) {
            <li className="nav-item">
              <a className="nav-link active" aria-current="page" href="/">Home</a>
            </li>
-           <li className="nav-item">
+           {/* <li className="nav-item">
              <a className="nav-link" href="/">{props.abouttext}</a>
-           </li>
+           </li> */}
          </ul>
          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
           <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
